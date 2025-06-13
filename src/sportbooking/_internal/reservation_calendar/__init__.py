c6f5d8d95ae4ed004854e63a197cd9c07687bced
Ivo Petkovic <ivo.petkovic@gmail.com>
2025-06-13 16:14:18 +0200

    update

--- src/sportbooking/_internal/reservation_calendar/__init__.py
+++ src/sportbooking/_internal/reservation_calendar/__init__.py
@@ -6,19 +6,5 @@ from sportbooking.reservation_calendar import UserReservationCalendar
 from sportbooking._internal.reservation_calendar.reservation_calendar import get_reservation_calendar as get_reservation_calendar_api
 
 
-class HourSlot(pydantic.BaseModel):
-    from_hour: int
-    to_hour: int
-
-    model_config = pydantic.ConfigDict(extra='ignore', frozen=True)
-
-
-class TimeSlot(pydantic.BaseModel):
-    date: datetime.date
-    hour_slot: HourSlot
-
-    model_config = pydantic.ConfigDict(extra='ignore', frozen=True)
-
-
 async def get_reservation_calendar(http_client: AsyncClient, token: str) -> UserReservationCalendar:
     return await get_reservation_calendar_api(http_client, token)
