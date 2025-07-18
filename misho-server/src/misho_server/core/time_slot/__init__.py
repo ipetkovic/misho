fa07eaa743768fec9a18d2356c93cc149a1945d0
Ivo Petkovic <ivo.petkovic@gmail.com>
2025-07-18 13:51:21 +0200

    include day name in time slot str representation

--- misho-server/src/misho_server/core/time_slot/__init__.py
+++ misho-server/src/misho_server/core/time_slot/__init__.py
@@ -15,4 +15,16 @@ class TimeSlot(pydantic.BaseModel):
         return datetime.datetime.combine(self.date, datetime.time(self.hour_slot.from_hour))
 
     def __str__(self):
-        return f"{self.date.strftime('%d.%m.%Y')} {self.hour_slot.from_hour:02d}:00 - {self.hour_slot.to_hour:02d}:00"
+        return f"{self._day_name}, {self.date.strftime('%d.%m.%Y')} {self.hour_slot.from_hour:02d}:00 - {self.hour_slot.to_hour:02d}:00"
+
+    def _day_name(self) -> str:
+        day_names = {
+            0: "Ponedjeljak",
+            1: "Utorok",
+            2: "Srijeda",
+            3: "Četvrtak",
+            4: "Petak",
+            5: "Subota",
+            6: "Nedjelja"
+        }
+        return day_names[self.date.weekday()]
